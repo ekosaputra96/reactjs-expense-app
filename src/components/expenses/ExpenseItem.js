f92b4f6a9e6962650e2ffa70b963fa6f0255ad7c
eko saputra <ekosaputra1331@gmail.com>
2022-09-07 10:23:49 +0700

    remove: changing title

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,26 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../utils/Card";
 
 const ExpenseItem = ({ date, price, title }) => {
-  const [mytitle, setTitle] = useState(title);
-  const clickHandler = () => {
-    setTitle("Updating...");
-    setTimeout(() => {
-      setTitle("Hello there !");
-    }, 3000);
-  };
+  // const [mytitle, setTitle] = useState(title);
+  // // const clickHandler = () => {
+  // //   setTitle("Updating...");
+  // //   setTimeout(() => {
+  // //     setTitle("Hello there !");
+  // //   }, 3000);
+  // };
 
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={date} />
         <div className="expense-item__description">
-          <h2>{mytitle}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">${price}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </Card>
     </li>
   );
